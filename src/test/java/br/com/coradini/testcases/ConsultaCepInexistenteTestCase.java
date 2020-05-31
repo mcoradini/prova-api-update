eff3712194e03b97496c515572cd65a2da51e93b
Marco Aurélio de Moraes Coradini <macoradini@gmail.com>
2020-05-31 18:41:15 -0300

    Atualizado projeto com framework Allure para geração de relatórios.

--- src/test/java/br/com/coradini/testcases/ConsultaCepInexistenteTestCase.java
+++ src/test/java/br/com/coradini/testcases/ConsultaCepInexistenteTestCase.java
@@ -1,11 +1,14 @@
 package br.com.coradini.testcases;
 
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
 
+@DisplayName("CEP Inexistente")
 public class ConsultaCepInexistenteTestCase {
 
     @BeforeEach
@@ -14,6 +17,10 @@ public class ConsultaCepInexistenteTestCase {
         basePath = "/ws/11111111/json";
     }
 
+    @DisplayName("Consulta API por um CEP inexistente")
+    @Description("Dado que o usuário inseri um CEP que não exista na base dos Correios\n" +
+                "Quando o serviço é consultado \n" +
+                "Então é retornada um atributo erro\n")
     @Test
     public void consultaCepInexistenteTest() {
         when().
--- src/test/java/br/com/coradini/testcases/ConsultaCepInvalidoTestCase.java
+++ src/test/java/br/com/coradini/testcases/ConsultaCepInvalidoTestCase.java
@@ -1,13 +1,16 @@
 package br.com.coradini.testcases;
 
+import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
+@DisplayName("CEP Inválido")
 public class ConsultaCepInvalidoTestCase {
 
     @BeforeEach
@@ -16,6 +19,10 @@ public class ConsultaCepInvalidoTestCase {
         basePath = "/ws/9106090/json";
     }
 
+    @DisplayName("Consulta API por um CEP Inválido")
+    @Description("Dado que o usuário inseri um CEP com formato inválido\n" +
+                "Quando o serviço é consultado \n" +
+                "Então é retornado uma mensagem de erro\n")
     @Test
     public void consultaCepInvalidoTest() {
         when().
